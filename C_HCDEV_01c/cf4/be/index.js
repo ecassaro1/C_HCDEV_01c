44507ac12db2c3a87e17879a758ee0a443b2823a
ecassaro1 <38324437+ecassaro1@users.noreply.github.com>
2022-09-09 14:31:03 -0300

    deletando o raiz

--- C_HCDEV_01c/cf4/be/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.get('/', function (req, res) {
-  res.setHeader('Content-Type', 'application/json');
-  res.end(JSON.stringify({ message: "Hello World Backend (cf4be)!!!" }));
-})
-
-app.listen(8080);
--- C_HCDEV_01c/cf5/be/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.get('/', function (req, res) {
-  res.setHeader('Content-Type', 'application/json');
-  res.end(JSON.stringify({ message: "Hello World Backend (cf5be)!!!" }));
-})
-
-app.listen(8080);
