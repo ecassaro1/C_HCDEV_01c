3924587eaadc143b89e95ed08ec6f84d75ec0eb3
Eric Cassaro <eric.cassaro@numenit.com>
2022-09-09 18:14:52 +0000

    voltando o cf5 a funcionar

--- cf5/be/index.js
+++ cf5/be/index.js
@@ -3,7 +3,49 @@ const app = express();
 
 app.get('/', function (req, res) {
   res.setHeader('Content-Type', 'application/json');
-  res.end(JSON.stringify({ message: "Hello World Backend (cf5be zuado)!!!" }));
+  res.end(JSON.stringify({ message: "Hello World Backend (cf5be)!!!" }));
 })
+
+app.get('/Ent1', async function (req, res) {
+    let rows = await getEnt1();
+    res.send(rows);
+})
+
+
+const getEnv = async ()=>{
+    var xsenv = require('@sap/xsenv');
+
+    xsenv.loadEnv();
+    let srv = xsenv.readServices();
+    //console.log(srv);
+    return srv;
+}
+
+const getEnt1 = async ()=>{
+    var hana = require('@sap/hana-client');
+
+    let vcap_services = await getEnv();
+
+    let credentials = vcap_services['cf5_hdi_db'].credentials;
+
+    var connOptions = {
+        serverNode: credentials.host+":"+credentials.port,
+        encrypt: 'true',
+        sslValidateCertificate: 'false',
+        uid: credentials.user,
+        pwd: credentials.password
+    };
+    
+    var connection = await hana.createConnection();
+
+    connection.connect(connOptions);
+
+    var sql = 'SELECT * FROM "025636FFD39143B4A610D9EF32B2BAF0"."ENT1"';
+
+    var rows = await connection.exec(sql);
+
+    return rows;
+}
+
 
 app.listen(8080);
